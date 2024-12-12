75ced9dfd9b6b5b83973a955cb91badb564ce86a
Nathan Gozun <114259029+Way2Farr@users.noreply.github.com>
2024-12-12 06:02:46 -0800

    Pressure Plates

--- Shifting/Assets/Level Editor Assets/LevelEditor.cs
+++ Shifting/Assets/Level Editor Assets/LevelEditor.cs
@@ -1,35 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class LevelEditor : MonoBehaviour
 {
     public ItemController[] itemButtons;
     public GameObject[] itemPrefabs;
-
     public GameObject[] itemGhost;
-
     [SerializeField] private Camera mainCamera;
-
     public int CurrentButtonPressed;
 
-
-    private void Update() {
-
-        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition); 
+    private void Update()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out RaycastHit raycastHit))
         {
-            if(Input.GetMouseButtonDown(0) && itemButtons[CurrentButtonPressed].Clicked){
+            if (Input.GetMouseButtonDown(0) && itemButtons[CurrentButtonPressed].Clicked)
+            {
                 itemButtons[CurrentButtonPressed].Clicked = false;
-                Instantiate(itemPrefabs[CurrentButtonPressed], raycastHit.point, Quaternion.identity);
+                GameObject newItem = Instantiate(itemPrefabs[CurrentButtonPressed], raycastHit.point, Quaternion.identity);
+
+                // Ensure the new item does not have a script that destroys it on collision
+                Collider newItemCollider = newItem.GetComponent<Collider>();
+                if (newItemCollider != null)
+                {
+                    newItemCollider.isTrigger = false;
+                }
 
                 Destroy(GameObject.FindGameObjectWithTag("ItemImage"));
+            }
         }
-        }
-
-    
     }
-    
 }
-
